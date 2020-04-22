9d8e47c96e4d6abf161e516ebcb0450debc076dd
Adrian Lam <adclam@ucdavis.edu>
2020-04-21 21:50:28 -0700

    First version test

--- BoardPrinterProject/src/board.py
+++ BoardPrinterProject/src/board.py
@@ -1,13 +1,26 @@
 # Place your Board class in this file
 from typing import *
 
-class Board:
-    def __init__(self,name,row,col,fill):
-    """
-    Param
-    """
-        self.row = input('Enter the number of rows for your board:')
-        self.col = input('Enter the number of columns for your board:')
-        self.blank = input('Enter the blank character to be used on your board')
+class Board(object):
+    def __init__(self,name:str,row:int,col:int,fill:str)->None:
+        """
+        Param name
+        Param row
+        Param col
+        Param what to fill with
+        """
+        self._name=name
+        self._row=row
+        self._col=col
+        self._fill=fill
+
+        fillboard()
+        showboard()
+
+    def fillboard(self):
+        pass
+
 
     def showboard
+        pass
+
--- BoardPrinterProject/src/boards_manager.py
+++ BoardPrinterProject/src/boards_manager.py
@@ -1,1 +1,19 @@
 # place your BoardsManager class in this file
+from src import board
+class BoardManager(object):
+    def initialboard(self):
+
+    def choice
+
+    def fillspot
+
+    def erasespot
+
+    def switchboard
+
+    def createboard
+
+    def quit
+
+    def saveboardnames
+
