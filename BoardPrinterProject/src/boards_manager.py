d517d5c7ad5f86319b9b892560b978a09d8870cf
Awen Li <32426838+BabyMochi@users.noreply.github.com>
2020-04-23 23:28:34 -0700

    Added .gitignore for .mypy_cache

--- BoardPrinterProject/src/boards_manager.py
+++ BoardPrinterProject/src/boards_manager.py
@@ -2,9 +2,13 @@
 from src import board
 class BoardManager(object):
     def initialboard(self):
+        list = []
+        expected_value = 0
+        result = get_index_of_min_item(list)
 
     def choice(self):
-
+        value = input (str\n)
+        print(f'{value}')
     def fillspot(self):
 
     def erasespot(self):
